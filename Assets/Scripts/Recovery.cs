fdc053dd0a230e97528f8ad464db4561e552bd97
AtobeRintaro <japanatobe@gmail.com>
2022-09-25 03:25:54 +0900

    [add]回復アイテムの追加

---
+++ Assets/Scripts/Recovery.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 取得したら回復するアイテム
+/// </summary>
+[RequireComponent(typeof(BoxCollider2D))]
+public class Recovery : ItemBase
+{
+    [Header("アイテムを取ったら回復するHP量")]
+    [SerializeField] int _recoveryHp = 1;
+
+    PlayerController _player;
+    SpriteRenderer _sprite;
+
+    public override void Activate()
+    {
+        Debug.Log("Active");
+        var playerObj = GameObject.FindGameObjectWithTag("Player");
+
+        if (playerObj)
+        {
+            _player = playerObj.GetComponent<PlayerController>();
+            if (_player)
+            {
+                Debug.Log("回復した");
+
+                if (_player.HP == _player.HpMax)
+                {
+                    return;
+                }
+                else
+                {
+                    _player.HP += _recoveryHp;
+
+                    if (_player.HP > _player.HpMax)
+                    {
+                        _player.HP = _player.HpMax;
+                    }
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
+}
